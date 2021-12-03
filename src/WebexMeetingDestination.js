e83e2864c44b23345d02244ebde61bb6151b3cd2
Adam Weeks <adweeks@cisco.com>
2021-12-03 17:18:44 -0500

    feat: add info for destination

--- src/WebexMeetingDestination.js
+++ src/WebexMeetingDestination.js
@@ -19,6 +19,10 @@ function WebexMeetingDestination({onSetDestination}) {
       <label htmlFor="WebexMeetingDestinationInput">Enter Webex Meeting Destination</label>
       <input id="WebexMeetingDestinationInput" type="text" placeholder="Enter Webex Meeting Destination" value={destination} onChange={handleDestinationChange}></input>
       <input type="button" onClick={handleSaveDestination} value="Save Destination"></input>
+      <div className="WebexMeetingDestinationNote">
+        <p>Webex meeting destinations can be email address, user ID, room ID or SIP address.</p>
+        <p>Note: SIP addresses are supported for Webex meetings SIP URIs and Webex cloud-registered devices. Webex Calling or third-party SIP URIs are not supported.</p>
+      </div>
     </div>
   )
 }
